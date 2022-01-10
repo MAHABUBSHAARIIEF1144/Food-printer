874983483aadcc4001a8df37283882cf5fe8bddf
MAHABUBSHAARIIEF1144 <61445247+MAHABUBSHAARIIEF1144@users.noreply.github.com>
2022-01-10 10:31:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Menu from './components/Menu';
+import Recipe from './components/Recipe';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Recipe />
+     <Menu />
+    </div>
+  );
+}
+
+export default App;
