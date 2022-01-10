0ceaafffd0d650b4347945ff04b5c794ac9305f9
MAHABUBSHAARIIEF1144 <61445247+MAHABUBSHAARIIEF1144@users.noreply.github.com>
2022-01-10 10:27:55 +0530

    Add files via upload

---
+++ components/Menu.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Menu.css";
+const Menu = () =>{
+    return(
+
+        <div className="menu">
+
+
+
+        </div>
+        
+    );
+}
+
+export default Menu;
